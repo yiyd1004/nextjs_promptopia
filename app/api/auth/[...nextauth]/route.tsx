9225466e6cbd5f2b90f8a9db84261aab2be3d436
Yong Yim <yiyd1004@gmail.com>
2023-08-31 22:28:30 -0700

    disable edge runtime on auth
    disable edge runtime on auth

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,7 +1,7 @@
 import { authOptions } from "@/utils/authOptions";
 import NextAuth from "next-auth";
 
-export const runtime = "edge";
+//export const runtime = "edge";
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
